93e38700fea5ef01010883f4389d385c7339b168
Matthew <mateogrigio@gmail.com>
2026-03-03 18:51:17 +0300

    some fixes

--- Sources/Modules/Auth/Protocols/AuthRepository.swift
+++ Sources/Modules/Auth/Protocols/AuthRepository.swift
@@ -10,7 +10,7 @@ import Foundation
 protocol AuthRepository {
     func login(email: String, password: String) async throws -> AuthResult
     func register(email: String, password: String, displayName: String?) async throws -> AuthResult
-    func verifyEmail(code: String) async throws -> VerificationResponse
+    func verifyEmail(code: String) async throws -> VerifyEmailResponse
     func resendVerificationCode(email: String) async throws -> ResendVerificationResponse
     func requestPasswordReset(email: String) async throws
     func verifyPasswordReset(email: String, code: String, newPassword: String) async throws
