155043219507eba9cccb35ca67d5d64739822c91
Matthew <mateogrigio@gmail.com>
2026-03-17 18:44:47 +0300

    code detail stub view

---
+++ Sources/Modules/CodeDetail/Views/CodeDetailStubViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CodeDetailStubViewController.swift
+//  TOTPly-ios
+//
+//  Created by Matthew on 17.03.2026.
+//
+
+import UIKit
+
+final class CodeDetailStubViewController: UIViewController {
+    private let itemId: String
+
+    private let label: UILabel = {
+        let l = UILabel()
+        l.font = .systemFont(ofSize: 18, weight: .medium)
+        l.textColor = .label
+        l.textAlignment = .center
+        l.numberOfLines = 0
+        l.translatesAutoresizingMaskIntoConstraints = false
+        return l
+    }()
+
+    init(itemId: String) {
+        self.itemId = itemId
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Детейл"
+
+        view.addSubview(label)
+        label.text = "Тут детейл для записи с айди \(itemId)"
+
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
+    }
+}
